cb3e9b16f62d1bb526cec7bdeecc3b8504b4f8bf
colin3dmax <colin3dmax@163.com>
2019-02-14 16:19:40 +0800

    更新24-26课

---
+++ Lesson24/demo/src/components/CContainer.vue
@@ -0,0 +1,37 @@
+<template>
+    <section class="c-container" :class="{
+        'is-vertical':direction==='vertical'
+    }">
+        <slot></slot>
+    </section>
+</template>
+
+<script>
+export default {
+    name: 'CContainer',
+    props:{
+        direction:{
+            type:String,
+            default:"vertical",
+            validator: function (value) {
+                return ['vertical', 'horizontal'].indexOf(value) !== -1
+            }
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+.c-container {
+    display: flex;
+    flex-direction: row;
+    flex: 1;
+    flex-basis: auto;
+    box-sizing: border-box;
+    min-width: 0; 
+    &.is-vertical {
+        flex-direction: column; 
+    }
+}
+</style>
+
